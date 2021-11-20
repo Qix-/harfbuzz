b4a9ef1b18e0fb37e77a1cc4657316b8a1aa640e
Behdad Esfahbod <behdad@behdad.org>
2021-11-20 12:02:47 -0700

    [unsafe-to-concat] Add to GPOS kerning

--- src/hb-ot-layout-gpos-table.hh
+++ src/hb-ot-layout-gpos-table.hh
@@ -1229,6 +1229,7 @@ struct PairSet
       buffer->idx = pos;
       return_trace (true);
     }
+    buffer->unsafe_to_concat (buffer->idx, pos + 1);
     return_trace (false);
   }
 
@@ -1574,6 +1575,8 @@ struct PairPosFormat2
     bool applied_second = valueFormat2.apply_value (c, this, v + len1, buffer->pos[skippy_iter.idx]);
     if (applied_first || applied_second)
       buffer->unsafe_to_break (buffer->idx, skippy_iter.idx + 1);
+    else
+      buffer->unsafe_to_concat (buffer->idx, skippy_iter.idx + 1);
 
     buffer->idx = skippy_iter.idx;
     if (len2)
